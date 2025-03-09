6a80e5df5a8e02ea97385aea430acddf4edf08b1
MagicalRobin <MagicalRobin4412316@gmail.com>
2025-03-09 11:23:50 -0500

    Add files via upload

---
+++ buttontest.js
@@ -0,0 +1,10 @@
+function test(){
+    alert("AHHHHH!!")
+    document.title = "Hi"
+
+    let text = document.getElementById("input1").value
+
+    document.getElementById("aaa").innerHTML =text
+
+}
+
